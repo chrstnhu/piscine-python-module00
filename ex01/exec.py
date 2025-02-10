42355e669373f841088f4c3dae418db1ace7d9d9
chrhu <chrhu@student.42.fr>
2025-02-10 12:23:56 +0100

    add comment

--- ex01/exec.py
+++ ex01/exec.py
@@ -1,17 +1,14 @@
 import sys
 
-# Check if argument is passed
 if len(sys.argv) > 1:
+    # Check if argument is an integer
     for i in range(1, len(sys.argv)):
         if sys.argv[i].isdigit():
-            try :
-                number = int(sys.argv[i])
                 print("Argument is an integer")
                 exit()
-            except ValueError:
-                print("Argument is not an integer")
-                exit()
+    # Join all arguments in one string
     result = " ".join(sys.argv[1:])
+    # Swap case and reverse the string
     result = result.swapcase()
     print(result[::-1])
     print("")
