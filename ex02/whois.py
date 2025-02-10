e56453450eb5f31f77d8c5c2269651ed2d3efa2a
chrhu <chrhu@student.42.fr>
2025-02-10 14:10:19 +0100

    add comment

--- ex02/whois.py
+++ ex02/whois.py
@@ -1,8 +1,10 @@
 import sys
 
+# Check if there is more than one argument
 if len(sys.argv) > 2:
     print("AssertionError: more than one argument is provided")
 elif len(sys.argv) == 2:
+    # Check if the argument is a number
     if sys.argv[1].isdigit():
         number = int(sys.argv[1])
         if number == 0:
