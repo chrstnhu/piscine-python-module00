766bd543b09dd8049fdd5681a460c52229ec6d84
chrhu <chrhu@student.42.fr>
2025-02-11 14:08:02 +0100

    add main and change to function

--- ex07/filterwords.py
+++ ex07/filterwords.py
@@ -2,11 +2,13 @@ import sys
 import string
 
 # Prints the list of words in string that contains more than N non-punctuation characters.
-if len(sys.argv) == 3:
-    if (not isinstance(sys.argv[1], str)) or sys.argv[1].isdigit() or (not sys.argv[2].isdigit()):
-        print("ERROR")
+def filterword() :
+    if (not isinstance(sys.argv[1], str)) or sys.argv[1].isdigit():
+        print("ERROR : need a STRING for argv[1]")
         sys.exit()
-    
+    elif not sys.argv[2].isdigit():
+        print("ERROR : need a DIGIT for argv[2]")
+        sys.exit()
     text = sys.argv[1] 
     max_nbr = int(sys.argv[2])
 
@@ -21,5 +23,11 @@ if len(sys.argv) == 3:
         if len(word) >= max_nbr:
             result.append(word)
     print(result)
-else:
-    print("ERROR")
+
+# Main
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+       filterword()
+    else:
+        print("Usage : python3 filterwords.py [string] [int]")
+    
